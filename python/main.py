82f3a034245fbd6f8a1429b52067217e5e98c28d
minji1160 <minji010611@gmail.com>
2023-12-19 05:05:05 +0100

    20231213
    edit(almost done..!)

--- python/main.py
+++ python/main.py
@@ -1,10 +1,18 @@
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from browser import launcher_for_data, launcher_for_table
 from consts import url, json_columns_data, table_population, table_undernourishment
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서의 요청을 허용하도록 설정 (보안상에 주의)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/maincounter-number")
 async def read_main():
